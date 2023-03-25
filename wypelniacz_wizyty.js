de6ac1b1fa1881c00ab587e08bedf9d63dfbb5ee
Jędrzej Kubala <jedrzej.kubala@gmail.com>
2023-03-25 16:35:12 +0100

    Add color override for buttons

--- wypelniacz_wizyty.js
+++ wypelniacz_wizyty.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wypelniacz_wizyty
 // @namespace    PrzystanMedyczna
-// @version      1.0
+// @version      1.1
 // @description  Wypełnij podstawowe pola wizyty w serum
 // @author       Jedrzej Kubala
 // @match        https://serum.com.pl/dpls/rm/ex.act
@@ -13,6 +13,7 @@
     'use strict';
     const KOMORKA_ZLECAJACA_ID = 290562;
     const MIEJSCA_ZABIEGU = { gabinet: 'GZ', teren: 'DCH' };
+    const COLOR_OVERRIDE = { green: 'cl-ovd-green', blue: 'cl-ovd-blue' }
 
     class Preset {
         constructor(data) {
@@ -21,6 +22,7 @@
             this.btn_id = data.btn_id;
             this.szablon_opisu = data.szablon_opisu;
             this.miejsce = data.miejsce;
+            this.kolor = data.kolor
         }
         callback = async (e) => await wypelnij_pola(this);
     }
@@ -46,20 +48,22 @@
                 { id: 14705, nazwa: 'Pomiar temperatury' },
                 { id: 14662, nazwa: 'EKG' },
             ],
-            miejsce: MIEJSCA_ZABIEGU.gabinet
+            miejsce: MIEJSCA_ZABIEGU.gabinet,
+            kolor: COLOR_OVERRIDE.blue,
         }),
         new Preset({
             btn_id: 'wyjazdowy_btn',
             btn_name: 'Wyjazd terenowy',
-            szablon_opisu: '',
+            szablon_opisu: 'W badaniu: \nZgłasza następujące objawy: ',
             czynnosci: [
                 { id: 23316, nazwa: 'Wyjazd terenowy' },
             ],
-            miejsce: MIEJSCA_ZABIEGU.teren
+            miejsce: MIEJSCA_ZABIEGU.teren,
+            kolor: COLOR_OVERRIDE.green,
         }),
     ]
 
-    const BTN_STYLE = "<style>.bubbly-button,body{font-family:Helvetica,Arial,sans-serif}body{font-size:16px;text-align:center;background-color:#f8faff}.bubbly-button{display:inline-block;font-size:1em;padding:1em 2em;margin-left:15px;-webkit-appearance:none;appearance:none;background-color:#ff0081;color:#fff;border-radius:4px;border:none;cursor:pointer;position:relative;transition:transform .1s ease-in,box-shadow .25s ease-in;box-shadow:0 2px 25px rgba(255,0,130,.5)}.bubbly-button:focus{outline:0}.bubbly-button:after,.bubbly-button:before{position:absolute;content:\"\";width:140%;height:100%;left:-20%;z-index:-1000;transition:1.5s ease-in-out;background-repeat:no-repeat}.bubbly-button:before{display:none;top:-75%;background-image:radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 20%,#ff0081 20%,transparent 30%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 10%,#ff0081 15%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%);background-size:10% 10%,20% 20%,15% 15%,20% 20%,18% 18%,10% 10%,15% 15%,10% 10%,18% 18%}.bubbly-button:after{display:none;bottom:-75%;background-image:radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 10%,#ff0081 15%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%);background-size:15% 15%,20% 20%,18% 18%,20% 20%,15% 15%,10% 10%,20% 20%}.bubbly-button:active{transform:scale(.9);background-color:#e60074;box-shadow:0 2px 25px rgba(255,0,130,.2)}.bubbly-button.animate:before{display:block;animation:2s ease-in-out forwards topBubbles}.bubbly-button.animate:after{display:block;animation:2s ease-in-out forwards bottomBubbles}@keyframes topBubbles{0%{background-position:5% 90%,10% 90%,10% 90%,15% 90%,25% 90%,25% 90%,40% 90%,55% 90%,70% 90%}50%{background-position:0 80%,0 20%,10% 40%,20% 0,30% 30%,22% 50%,50% 50%,65% 20%,90% 30%}100%{background-position:0 70%,0 10%,10% 30%,20% -10%,30% 20%,22% 40%,50% 40%,65% 10%,90% 20%;background-size:0 0,0 0,0 0,0 0,0 0,0 0}}@keyframes bottomBubbles{0%{background-position:10% -10%,30% 10%,55% -10%,70% -10%,85% -10%,70% -10%,70% 0}50%{background-position:0 80%,20% 80%,45% 60%,60% 100%,75% 70%,95% 60%,105% 0}100%{background-position:0 90%,20% 90%,45% 70%,60% 110%,75% 80%,95% 70%,110% 10%;background-size:0 0,0 0,0 0,0 0,0 0,0 0}}</style>"
+    const BTN_STYLE = "<style>.bubbly-button,body{font-family:Helvetica,Arial,sans-serif}body{font-size:16px;text-align:center;background-color:#f8faff}.cl-ovd-green{background-color:#61ce70!important;box-shadow:0 2px 25px rgba(97,206,112,.5)!important}.cl-ovd-blue{background-color:#6ec1e4!important;box-shadow:0 2px 25px rgba(110,193,228,.5)!important}.bubbly-button{display:inline-block;font-size:1em;padding:1em 2em;margin-left:15px;-webkit-appearance:none;appearance:none;background-color:#ff0081;color:#fff;border-radius:4px;border:none;cursor:pointer;position:relative;transition:transform .1s ease-in,box-shadow .25s ease-in;box-shadow:0 2px 25px rgba(255,0,130,.5)}.bubbly-button:focus{outline:0}.bubbly-button:after,.bubbly-button:before{position:absolute;content:\"\";width:140%;height:100%;left:-20%;z-index:-1000;transition:.5s ease-in-out;background-repeat:no-repeat}.bubbly-button:before{display:none;top:-75%;background-image:radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 20%,#ff0081 20%,transparent 30%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 10%,#ff0081 15%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%);background-size:10% 10%,20% 20%,15% 15%,20% 20%,18% 18%,10% 10%,15% 15%,10% 10%,18% 18%}.bubbly-button:after{display:none;bottom:-75%;background-image:radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,transparent 10%,#ff0081 15%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%),radial-gradient(circle,#ff0081 20%,transparent 20%);background-size:15% 15%,20% 20%,18% 18%,20% 20%,15% 15%,10% 10%,20% 20%}.bubbly-button:active{transform:scale(.9);background-color:#e60074;box-shadow:0 2px 25px rgba(255,0,130,.2)}.bubbly-button.animate:before{display:block;animation:.75s ease-in-out forwards topBubbles}.bubbly-button.animate:after{display:block;animation:.75s ease-in-out forwards bottomBubbles}@keyframes topBubbles{0%{background-position:5% 90%,10% 90%,10% 90%,15% 90%,25% 90%,25% 90%,40% 90%,55% 90%,70% 90%}50%{background-position:0 80%,0 20%,10% 40%,20% 0,30% 30%,22% 50%,50% 50%,65% 20%,90% 30%}100%{background-position:0 70%,0 10%,10% 30%,20% -10%,30% 20%,22% 40%,50% 40%,65% 10%,90% 20%;background-size:0 0,0 0,0 0,0 0,0 0,0 0}}@keyframes bottomBubbles{0%{background-position:10% -10%,30% 10%,55% -10%,70% -10%,85% -10%,70% -10%,70% 0}50%{background-position:0 80%,20% 80%,45% 60%,60% 100%,75% 70%,95% 60%,105% 0}100%{background-position:0 90%,20% 90%,45% 70%,60% 110%,75% 80%,95% 70%,110% 10%;background-size:0 0,0 0,0 0,0 0,0 0,0 0}}</style>"
 
     var animateButton = function (e) {
         e.preventDefault;
@@ -118,7 +122,8 @@
     }
 
     function addButton(config, container) {
-        const button = '<a class="bubbly-button" id="' + config.btn_id + '" href="#">' + config.btn_name + '</a>'
+        const color_override = config.kolor ? ' ' + config.kolor : '';
+        const button = `<a class="bubbly-button${color_override}" id="${config.btn_id}" href="#">${config.btn_name}</a>`
         container.insertAdjacentHTML('beforeend', button);
         $(config.btn_id).addEventListener('click', config.callback, false);
 
